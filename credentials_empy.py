324e38cdea8a3e747f474536b4a5996b4159e1cc
NjotuAwasi <njotuawasi@gmail.com>
2022-12-08 12:33:57 +0100

    initial commit
    Twitter bot that helps you post tweets, retweets and follows users

---
+++ credentials_empy.py
@@ -0,0 +1,7 @@
+#  this is just  a sample config file.You need to edit this file.You need to get these details from your Twitter app settings.
+# You should also rename this file to credentials.py
+
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
